a7b6c40d693442645ef18a7a03673799e1e19a45
likeke <likek@outlook.com>
2016-09-04 10:37:18 +0800

    修复左侧自动焦点在刚进页面时无效的bug，并去除点击事件(不需要)

--- app/components/auto-focus.js
+++ app/components/auto-focus.js
@@ -1,18 +1,16 @@
 var autofocus=angular.module('module_auto_focus',[]);
 autofocus.directive('autoFocus',['$location',function($location){
-	var path=$location.path();
 	return {
 		restrict:'A',
 		link:function($scope,elem,attr,controller){
-			var aEle=elem.children().attr('href');
-			var currentPath=aEle.substr(1);
-//			console.log(currentPath);
-			if(path.startsWith(currentPath)){
-				elem.addClass('active');
-			}
-			elem.on('click',function(){
-				elem.parent().children().removeClass('active');
-				elem.addClass('active');
+			$scope.$location=$location;
+			$scope.$watch('$location.path()',function(now){
+				var aEle=elem.children().attr('href');
+				var currentPath=aEle.substr(1);
+				if(now.startsWith(currentPath)){
+					elem.parent().children().removeClass('active');
+					elem.addClass('active');
+				}
 			})
 		}
 	}
